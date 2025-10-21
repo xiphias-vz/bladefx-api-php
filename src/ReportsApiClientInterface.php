17ebf8bfaa8f5d592923a222c1fa2e0aea2261a2
MihaelBlazek <mihael.blazek@xiphias.hr>
2025-10-21 14:02:30 +0200

    removed unnecessary comments

--- src/ReportsApiClientInterface.php
+++ src/ReportsApiClientInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Xiphias\BladeFxApi;
 
-use Xiphias\BladeFxApi\DTO\AbstractTransfer;
-use Xiphias\BladeFxApi\DTO\BladeFxAuthenticationRequestTransfer;
 use Xiphias\BladeFxApi\DTO\BladeFxAuthenticationResponseTransfer;
 use Xiphias\BladeFxApi\DTO\BladeFxGetCategoriesListRequestTransfer;
 use Xiphias\BladeFxApi\DTO\BladeFxCategoriesListResponseTransfer;
@@ -17,13 +15,11 @@ use Xiphias\BladeFxApi\DTO\BladeFxGetReportsListRequestTransfer;
 use Xiphias\BladeFxApi\DTO\BladeFxGetReportsListResponseTransfer;
 use Xiphias\BladeFxApi\DTO\BladeFxSetFavoriteReportRequestTransfer;
 use Xiphias\BladeFxApi\DTO\BladeFxSetFavoriteReportResponseTransfer;
-use Xiphias\BladeFxApi\DTO\BladeFxTokenTransfer;
 
 interface ReportsApiClientInterface
 {
     /**
      * @return BladeFxAuthenticationResponseTransfer|null
-     * @throws \DateMalformedStringException
      */
     public function sendAuthenticateUserRequest(): ?BladeFxAuthenticationResponseTransfer;
 
@@ -45,11 +41,6 @@ interface ReportsApiClientInterface
      */
     public function sendSetFavoriteReportRequest(?BladeFxSetFavoriteReportRequestTransfer $bladeFxSetFavoriteReportRequestTransfer): BladeFxSetFavoriteReportResponseTransfer;
 
-//
-//    public function sendGetReportByFormatRequest(
-//        BladeFxGetReportByFormatRequestTransfer $requestTransfer
-//    ): BladeFxGetReportByFormatResponseTransfer;
-
     /**
      * @param BladeFxGetReportParamFormRequestTransfer|null $reportsParamFormRequestTransfer
      * @return BladeFxGetReportParamFormResponseTransfer
@@ -61,19 +52,4 @@ interface ReportsApiClientInterface
      * @return BladeFxReportPreviewResponseTransfer
      */
     public function sendGetReportPreviewRequest(BladeFxGetReportPreviewRequestTransfer $bladeFxReportPreviewRequestTransfer): BladeFxReportPreviewResponseTransfer;
-
-//
-//    public function sendGetReportParameterListRequest(
-//        BladeFxGetReportParameterListRequestTransfer $requestTransfer
-//    ): BladeFxGetReportParameterListResponseTransfer;
-//
-//
-//    public function sendCreateOrUpdateUserOnBfxRequest(
-//        BladeFxCreateOrUpdateUserRequestTransfer $requestTransfer
-//    ): BladeFxCreateOrUpdateUserResponseTransfer;
-//
-//
-//    public function sendUpdatePasswordOnBladeFxRequest(
-//        BladeFxUpdatePasswordRequestTransfer $requestTransfer
-//    ): BladeFxUpdatePasswordResponseTransfer;
 }
