548187617a506991ba2c028ffeaf5d071d086682
MihaelBlazek <mihael.blazek@xiphias.hr>
2025-10-29 08:42:27 +0100

    adjustments

--- src/DTO/AbstractTransfer.php
+++ src/DTO/AbstractTransfer.php
@@ -54,6 +54,13 @@ class AbstractTransfer
                 $this->$property = new ArrayObject();
             }
         }
+
+        if (isset($this->baseUrl) && $this->getBaseUrl() === '') {
+            unset($this->baseUrl);
+        }
+        if (isset($this->errorMessage) && $this->getErrorMessage() === null) {
+            unset($this->errorMessage);
+        }
     }
 
     /**
