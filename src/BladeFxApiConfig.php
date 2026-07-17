97d69135ed0f0fc0a5e0a254aa0e3e975a3b2cf5
javornovosel <javor.novosel@gmail.com>
2026-07-17 15:52:00 +0200

    removed comment

--- src/BladeFxApiConfig.php
+++ src/BladeFxApiConfig.php
@@ -44,8 +44,6 @@ class BladeFxApiConfig
     /**
      * @var string
      */
-    #public const GET_REPORT_PREVIEW_API_RESOURCE = '/api/ReportData/GetEncryptedData';
-
     public const GET_REPORT_PREVIEW_API_RESOURCE = '/api/ReportData/GetReportPreviewUrl';
 
     /**
